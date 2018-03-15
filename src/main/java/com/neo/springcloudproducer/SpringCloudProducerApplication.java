97f66c96e77975264e2867f76d213f91b11d9144
徐博 <17714336067@163.com>
2018-03-15 10:16:02 +0800

    Initial commit

---
+++ src/main/java/com/neo/springcloudproducer/SpringCloudProducerApplication.java
@@ -0,0 +1,16 @@
+package com.neo.springcloudproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudProducerApplication.class, args);
+	}
+}
+
+
